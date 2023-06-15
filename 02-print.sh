bdd77c86565d8eb018e1f66de39aa6077d331443
nani <nani@gmail.com>
2023-06-15 14:19:57 -0400

    Second Shell Script

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,7 @@
 echo Hello World! This is DevOps Course.
+
+echo -e "\e[31m Vinay Reddy\e[0m"
+echo -e "\e[31m Baji Babu\e[0m"
+echo -e "\e[31m Mallikarjun\e[0m"
+echo -e "\e[31m Charan\e[0m"
+echo -e "\e[31m Durga\e[0m"
